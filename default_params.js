f0e43fdd09f541d25a828f295f64b95513908a51
Guillaume Pinot <guillaume.pinot@tremplin-utc.net>
2015-04-17 12:41:39 +0200

    update urls to use http instead of https to fix "blocked mixed content"

--- default_params.js
+++ default_params.js
@@ -1,3 +1,3 @@
 var navitia_api_key ='get an api key at http://auth.navitia.io'
 var navitia_coverage = 'fr-idf'
-var navitia_url = 'http://api.navitia.io' //no trailing slash
+var navitia_url = 'https://api.navitia.io' //no trailing slash
